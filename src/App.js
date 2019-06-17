4b9e6148fb0b6039217b0eec2c5c7b93c08b3d8b
Karim Bouchekoura <karim@MBP-de-Karim.lan>
2019-06-16 21:26:00 -1000

    changed app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           OKxxxLLL
+           OK
         </p>
         <a
           className="App-link"
